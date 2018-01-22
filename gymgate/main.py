a8d933733f479e66d538d7a50b8bcf1d5c561582
Robert-Jan Keizer <robertjankeizer@gmail.com>
2018-01-22 11:40:29 +0100

    Added mysql connector

--- gymgate/main.py
+++ gymgate/main.py
@@ -18,7 +18,6 @@ config = {
 AUTOMAAT_ID = 1
 
 cnx = mysql.connector.connect(**config)
-
 continue_reading = True
 
 
@@ -40,42 +39,27 @@ MIFAREReader = MFRC522.MFRC522()
 
 GPIO.setmode(GPIO.BOARD)
 
-red = LED(GPIO, 12)
+LED_red = LED(GPIO, 12)
 
-# Welcome message
-print("Welcome to the MFRC522 data read example")
+print("Gymgate scanner")
 print("Press Ctrl-C to stop.")
 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
 while continue_reading:
     # Scan for cards
     (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # Get two buffered cursors
-    cursor_query_user = cnx.cursor(buffered=True)
-    cursor_insert_activity = cnx.cursor(buffered=True)
-
-    # # Iterate through the result of curA
-    # for (emp_no, salary, from_date, to_date) in curA:
-    #     # Update the old and insert the new salary
-    #     new_salary = int(round(salary * Decimal('1.15')))
-    #     curB.execute(update_old_salary, (tomorrow, emp_no, from_date))
-    #     curB.execute(insert_new_salary,
-    #                  (emp_no, tomorrow, date(9999, 1, 1, ), new_salary))
-    #
-    #     # Commit the changes
-    #     cnx.commit()
-
-    # If a card is found
     if status == MIFAREReader.MI_OK:
         print("Card detected")
+
+    cursor_query_user = cnx.cursor(buffered=True)
+    cursor_insert_activity = cnx.cursor(buffered=True)
 
     # Get the UID of the card
     (status, uid) = MIFAREReader.MFRC522_Anticoll()
 
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
-        red.turn_on()
+        LED_red.turn_on()
 
         number = str(uid[0]) + "." + str(uid[1]) + "." + str(uid[2]) + "." + str(uid[3])
 
@@ -83,7 +67,7 @@ while continue_reading:
             "SELECT * FROM gebruikers WHERE pasnummer = %s"
         )
 
-        cursor_query_user.execute(query_user, (number, ))
+        cursor_query_user.execute(query_user, (number,))
         data_query_user = cursor_query_user.fetchall()
 
         print(data_query_user[0])
