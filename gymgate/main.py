ad0fec713afc33610a1d018107699d974b8c8bd8
Robert-Jan Keizer <robertjankeizer@gmail.com>
2018-01-22 10:52:07 +0100

    Added mysql connector

--- gymgate/main.py
+++ gymgate/main.py
@@ -13,22 +13,20 @@ config = {
   'host': '192.168.42.7',
   'database': 'sportschool',
   'raise_on_warnings': True,
-  'use_pure': False,
 }
-
 
 cnx = mysql.connector.connect(**config)
 
-cnx.close()
 
 continue_reading = True
-
 
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal, frame):
     global continue_reading
     print("Ctrl+C captured, ending read.")
     continue_reading = False
+
+    cnx.close()
     GPIO.cleanup()
 
 
@@ -51,6 +49,21 @@ while continue_reading:
     # Scan for cards
     (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
+    # Get two buffered cursors
+    curA = cnx.cursor(buffered=True)
+    curB = cnx.cursor(buffered=True)
+
+    # # Iterate through the result of curA
+    # for (emp_no, salary, from_date, to_date) in curA:
+    #     # Update the old and insert the new salary
+    #     new_salary = int(round(salary * Decimal('1.15')))
+    #     curB.execute(update_old_salary, (tomorrow, emp_no, from_date))
+    #     curB.execute(insert_new_salary,
+    #                  (emp_no, tomorrow, date(9999, 1, 1, ), new_salary))
+    #
+    #     # Commit the changes
+    #     cnx.commit()
+
     # If a card is found
     if status == MIFAREReader.MI_OK:
         print("Card detected")
@@ -61,6 +74,13 @@ while continue_reading:
     # If we have the UID, continue
     if status == MIFAREReader.MI_OK:
         red.turn_on()
+        query_user = (
+            "SELECT user_id FROM gebruikers WHERE pasnr IS %s"
+        )
+        curA.execute(query_user, (str(uid[0]) + "." + str(uid[1]) + "." + str(uid[2]) + "." + str(uid[3])))
+
+        print(user_id)
+
         print(str(uid[0]) + "." + str(uid[1]) + "." + str(uid[2]) + "." + str(uid[3]))
     else:
         red.turn_off()
