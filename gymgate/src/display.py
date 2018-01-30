e64c955bc77413221d9830008cde6c11682a90a2
Robert-Jan Keizer <robertjankeizer@gmail.com>
2018-01-30 13:22:42 +0100

    Added repository

--- gymgate/src/display.py
+++ gymgate/src/display.py
@@ -7,6 +7,7 @@ class Display:
         self.lcd = CharLCD(cols=16, rows=2, pin_rs=16, pin_e=36, pins_data=[38, 32, 11, 37], numbering_mode=GPIO.BOARD)
 
     def show_message(self, text):
+        self.clear()
         self.lcd.write_string(text)
 
     def clear(self):
