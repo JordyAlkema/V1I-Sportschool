ed7253c11ef98fe66c826f9c67797d1e233dd753
Robert-Jan Keizer <robertjankeizer@gmail.com>
2018-01-30 11:19:39 +0100

    Added repository

--- gymgate/main.py
+++ gymgate/main.py
@@ -19,7 +19,7 @@ class GymGate:
     def __init__(self):
         self.gymgate_repository = gymgate_repository.GymgateRepository()
         self.is_running = True
-        self.RFID = RFID(pin_mode=GPIO.BCM)
+        self.RFID = RFID(pin_mode=GPIO.BCM, bus=0, device=1)
         self.display = Display()
         self.LED_green = LED(GPIO, 9)
         self.LED_red = LED(GPIO, 18)
